1fecb3ccbeeda528c9b84731da22cf671bf7dc3e
anirud <127181241+Anirudpagare20@users.noreply.github.com>
2024-04-08 00:38:28 +0530

    add all files

---
+++ client/src/pages/About.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+    <h1 className='text-4xl md:text-5xl font-bold mb-6 text-gray-900'>Welcome to Nashik Estate</h1>
+    <p className='mb-6 text-gray-800'>
+      Sahand Estate stands at the forefront of the real estate industry, specializing in facilitating seamless transactions for our valued clients. With a focus on integrity, innovation, and excellence, we redefine the real estate experience.
+    </p>
+    <p className='mb-6 text-gray-800'>
+      Our dedicated team of seasoned professionals possesses a wealth of expertise and a deep understanding of the market, ensuring unparalleled service tailored to your needs. Whether you're looking to buy, sell, or rent, we are your trusted partner every step of the way.
+    </p>
+    <p className='mb-6 text-gray-800'>
+      At Sahand Estate, we embrace the latest technologies and industry trends to deliver optimal results. Our commitment to your success drives us to exceed expectations and achieve remarkable outcomes, making your real estate journey truly exceptional.
+    </p>
+  </div>
+  
+  )
+}
